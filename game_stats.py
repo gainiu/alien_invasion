e99bb58c6f073c2e93fab16e4ee8dbc5d0578f48
gainiu <276647369@qq.com>
2020-10-23 08:21:21 +0800

    update

--- game_stats.py
+++ game_stats.py
@@ -1,7 +1,7 @@
 class GameStats():
     def __init__(self,ai_settings):
         '''初始化统计信息'''
-        self.ai_settings=ai_settings
+        self.ai_settings=ai_settings        
         self.reset_stats()
         #游戏刚启动时处于非活动状态
         self.game_active=False
@@ -9,3 +9,4 @@ class GameStats():
     def reset_stats(self):
         '''初始化在游戏运行期间可能变化的统计信息'''
         self.ship_left=self.ai_settings.ship_limit
+        self.score=0
---
+++ scoreboard.py
@@ -0,0 +1,6 @@
+import pygame
+
+class ScoreBoard():
+    '''显示得分信息的类'''
+    def __init__(self):
+        super().__init__()
